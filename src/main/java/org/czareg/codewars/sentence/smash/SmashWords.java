c0190643e32f909068ff40f9fcee5919b13d3a3b
Czareg <czarecoo@gmail.com>
2023-12-11 13:30:42 +0100

    Done 1 kata

---
+++ src/main/java/org/czareg/codewars/sentence/smash/SmashWords.java
@@ -0,0 +1,20 @@
+package org.czareg.codewars.sentence.smash;
+
+import lombok.NonNull;
+import lombok.experimental.UtilityClass;
+
+/*
+Write a function that takes an array of words and smashes them together into a sentence and returns the sentence.
+You can ignore any need to sanitize words or add punctuation, but you should add spaces between each word.
+Be careful, there shouldn't be a space at the beginning or the end of the sentence!
+
+Example
+['hello', 'world', 'this', 'is', 'great']  =>  'hello world this is great'
+ */
+@UtilityClass
+public class SmashWords {
+
+    public static String smash(@NonNull String... words) {
+        return String.join(" ", words);
+    }
+}
---
+++ src/test/java/org/czareg/codewars/sentence/smash/SmashWordsTest.java
@@ -0,0 +1,34 @@
+package org.czareg.codewars.sentence.smash;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class SmashWordsTest {
+
+    @Test
+    void validate() {
+        assertEquals("Bilal Djaghout", SmashWords.smash("Bilal", "Djaghout"));
+        assertEquals("hello world", SmashWords.smash("hello", "world"));
+        assertEquals("hello amazing world", SmashWords.smash("hello", "amazing", "world"));
+    }
+
+    @Test
+    void validateEmpty() {
+        assertEquals("", SmashWords.smash());
+    }
+
+    @Test
+    void validateNull() {
+        assertEquals("null", SmashWords.smash((String) null));
+        assertThrows(NullPointerException.class, () -> SmashWords.smash((String[]) null));
+    }
+
+    @Test
+    void validateOneWord() {
+        assertEquals("Bilal", SmashWords.smash("Bilal"));
+        assertEquals("Test", SmashWords.smash("Test"));
+    }
+
+}
