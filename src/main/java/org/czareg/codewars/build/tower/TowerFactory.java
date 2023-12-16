2aeffbd7c87970112f111fa2e4ef70b0bfa344f9
Czareg <czarecoo@gmail.com>
2023-12-16 22:01:28 +0100

    Done 1 kata

---
+++ src/main/java/org/czareg/codewars/build/tower/TowerFactory.java
@@ -0,0 +1,49 @@
+package org.czareg.codewars.build.tower;
+
+import lombok.experimental.UtilityClass;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+Build a pyramid-shaped tower, as an array/list of strings, given a positive integer number of floors. A tower block is represented with "*" character.
+
+For example, a tower with 3 floors looks like this:
+
+[
+  "  *  ",
+  " *** ",
+  "*****"
+]
+And a tower with 6 floors looks like this:
+
+[
+  "     *     ",
+  "    ***    ",
+  "   *****   ",
+  "  *******  ",
+  " ********* ",
+  "***********"
+]
+ */
+@UtilityClass
+public class TowerFactory {
+
+    public static String[] floors(int numberOfFloors) {
+        if (numberOfFloors <= 0) {
+            return new String[0];
+        }
+        int charactersPerFloor = numberOfFloors * 2 - 1;
+        List<String> tower = new ArrayList<>();
+        for (int floor = 1; floor <= numberOfFloors; floor++) {
+            int starCharactersCount = floor * 2 - 1;
+            int paddingCharactersOnEachSideCount = (charactersPerFloor - starCharactersCount) / 2;
+            tower.add(buildFloorString(paddingCharactersOnEachSideCount, starCharactersCount));
+        }
+        return tower.toArray(new String[0]);
+    }
+
+    private static String buildFloorString(int paddingCount, int starCount) {
+        return "%1$s%2$s%1$s".formatted(" ".repeat(paddingCount), "*".repeat(starCount));
+    }
+}
---
+++ src/test/java/org/czareg/codewars/build/tower/TowerFactoryTest.java
@@ -0,0 +1,74 @@
+package org.czareg.codewars.build.tower;
+
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.Random;
+import java.util.stream.IntStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class TowerFactoryTest {
+
+    public static char BLOCK_CHAR = '*';
+    public static char SPACE_CHAR = ' ';
+
+    @Test
+    void basicTests() {
+        assertEquals(String.join(",", "*"), String.join(",", buildTower(1)));
+        assertEquals(String.join(",", " * ", "***"), String.join(",", buildTower(2)));
+        assertEquals(String.join(",", "  *  ", " *** ", "*****"), String.join(",", buildTower(3)));
+    }
+
+    @Test
+    void randomTests() {
+        Random rand = new Random();
+        int[] seq = IntStream.range(0, 100).toArray();
+        for (int r = 0; r < 100; r++) {
+            for (int p = 0; p < 100; p++) {
+                if (rand.nextInt(2 + 1) == 0) {
+                    int temp = seq[r];
+                    seq[r] = seq[p];
+                    seq[p] = temp;
+                }
+            }
+        }
+
+        for (int r = 0; r < 100; r++) {
+            int n = seq[r];
+            assertEquals(String.join(",", testTowerBuilder(n)), String.join(",", buildTower(n)));
+        }
+    }
+
+    private String[] buildTower(int level) {
+        Method method = null;
+        try {
+            method = TowerFactory.class.getDeclaredMethod("floors", int.class);
+        } catch (NoSuchMethodException ignore) {
+        }
+
+        try {
+            if (method == null) {
+                method = TowerFactory.class.getDeclaredMethod("floors", int.class);
+            }
+            return (String[]) method.invoke(null, level);
+        } catch (Exception cause) {
+            throw new RuntimeException(cause);
+        }
+    }
+
+    private static String[] testTowerBuilder(int nFloors) {
+        String[] lines = new String[nFloors];
+        for (int i = 1; i <= nFloors; i++) {
+            lines[i - 1] = (fillString(TowerFactoryTest.SPACE_CHAR, nFloors - i) + fillString(TowerFactoryTest.BLOCK_CHAR, i * 2 - 1) + fillString(TowerFactoryTest.SPACE_CHAR, nFloors - i));
+        }
+        return lines;
+    }
+
+    private static String fillString(char character, int length) {
+        char[] chars = new char[length];
+        Arrays.fill(chars, character);
+        return new String(chars);
+    }
+}
