c35fc12f4f6da8ff0fe5efc429280492d5c337e2
Czareg <czarecoo@gmail.com>
2023-12-25 22:38:18 +0100

    Done 1 kata

---
+++ src/test/java/org/czareg/codewars/ten/min/walk/TenMinWalkTest.java
@@ -0,0 +1,17 @@
+package org.czareg.codewars.ten.min.walk;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class TenMinWalkTest {
+
+    @Test
+    void test() {
+        assertTrue(TenMinWalk.isValid(new char[]{'n', 's', 'n', 's', 'n', 's', 'n', 's', 'n', 's'}));
+        assertFalse(TenMinWalk.isValid(new char[]{'w', 'e', 'w', 'e', 'w', 'e', 'w', 'e', 'w', 'e', 'w', 'e'}));
+        assertFalse(TenMinWalk.isValid(new char[]{'w'}));
+        assertFalse(TenMinWalk.isValid(new char[]{'n', 'n', 'n', 's', 'n', 's', 'n', 's', 'n', 's'}));
+    }
+}
