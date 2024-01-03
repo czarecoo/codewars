f125bb4640fafbfb6ead22ba25c97914e88dd0b1
Czareg <czarecoo@gmail.com>
2024-01-03 20:51:29 +0100

    Done 1 kata

--- src/main/java/molecule/to/atoms/ParseMolecule.java
+++ src/main/java/molecule/to/atoms/ParseMolecule.java
@@ -2,22 +2,19 @@ package molecule.to.atoms;
 
 import lombok.experimental.UtilityClass;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.NoSuchElementException;
+import java.util.stream.Stream;
 
 /*
 For a given chemical formula represented by a string, count the number of atoms of each element contained in the molecule and return an object (associative array in PHP, Dictionary<string, int> in C#, Map<String,Integer> in Java).
 
 For example:
-
-String water = "H2O";
-parseMolecule.getAtoms(water); // return [H: 2, O: 1]
-
-String magnesiumHydroxide = "Mg(OH)2";
-parseMolecule.getAtoms(magnesiumHydroxide); // return ["Mg": 1, "O": 2, "H": 2]
-
-String fremySalt = "K4[ON(SO3)2]2";
-parseMolecule.getAtoms(fremySalt); // return ["K": 4, "O": 14, "N": 2, "S": 4]
-
+parseMolecule.getAtoms("H2O"); // return [H: 2, O: 1]
+parseMolecule.getAtoms("Mg(OH)2"); // return ["Mg": 1, "O": 2, "H": 2]
+parseMolecule.getAtoms("K4[ON(SO3)2]2"); // return ["K": 4, "O": 14, "N": 2, "S": 4]
 parseMolecule.getAtoms("pie"); // throw an IllegalArgumentException
 As you can see, some formulas have brackets in them. The index outside the brackets tells you that you have to multiply count of each atom inside the bracket on this index. For example, in Fe(NO3)2 you have one iron atom, two nitrogen atoms and six oxygen atoms.
 
@@ -26,82 +23,108 @@ Note that brackets may be round, square or curly and can also be nested. Index a
 @UtilityClass
 class ParseMolecule {
 
-    private static final List<String> SYMBOLS = Arrays.asList(
-            "H", "He", "Li", "Be", "B", "C", "N", "O", "F", "Ne",
-            "Na", "Mg", "Al", "Si", "P", "S", "Cl", "Ar", "K", "Ca",
-            "Sc", "Ti", "V", "Cr", "Mn", "Fe", "Co", "Ni", "Cu", "Zn",
-            "Ga", "Ge", "As", "Se", "Br", "Kr", "Rb", "Sr", "Y", "Zr",
-            "Nb", "Mo", "Tc", "Ru", "Rh", "Pd", "Ag", "Cd", "In", "Sn",
-            "Sb", "Te", "I", "Xe", "Cs", "Ba", "La", "Ce", "Pr", "Nd",
-            "Pm", "Sm", "Eu", "Gd", "Tb", "Dy", "Ho", "Er", "Tm", "Yb",
-            "Lu", "Hf", "Ta", "W", "Re", "Os", "Ir", "Pt", "Au", "Hg",
-            "Tl", "Pb", "Bi", "Po", "At", "Rn", "Fr", "Ra", "Ac", "Th",
-            "Pa", "U", "Np", "Pu", "Am", "Cm", "Bk", "Cf", "Es", "Fm",
-            "Md", "No", "Lr", "Rf", "Db", "Sg", "Bh", "Hs", "Mt", "Ds",
-            "Rg", "Cn", "Nh", "Fl", "Mc", "Lv", "Ts", "Og");
+    private static final char OPENING_ROUND_BRACKET = '(';
+    private static final char OPENING_SQUARE_BRACKET = '[';
+    private static final char OPENING_CURLY_BRACKET = '{';
+    private static final char CLOSING_ROUND_BRACKET = ')';
+    private static final char CLOSING_SQUARE_BRACKET = ']';
+    private static final char CLOSING_CURLY_BRACKET = '}';
 
     public static Map<String, Integer> getAtoms(String formula) {
         Map<String, Integer> hashMap = new HashMap<>();
-
         FormulaIterator formulaIterator = new FormulaIterator(formula);
         while (formulaIterator.hasNext()) {
             String token = formulaIterator.next();
-            if (isMulecule(token)) {
-                Map.Entry<String, Integer> elementWithCount = splitMolecule(token);
-                String elementName = elementWithCount.getKey();
-                Integer elementCount = elementWithCount.getValue();
-                Integer old = hashMap.getOrDefault(elementName, 0);
-                hashMap.put(elementName, old + elementCount);
-            } else {
-                //bracket
-                Map.Entry<String, Integer> formulaWithCount = splitFormula(token);
+            if (containsInnerFormula(token)) {
+                Map.Entry<String, Integer> formulaWithCount = stripAndSplitFormulaFromItsCount(token);
                 String innerFormula = formulaWithCount.getKey();
                 int multiplier = formulaWithCount.getValue();
-
                 Map<String, Integer> innerHashMap = getAtoms(innerFormula);
-                innerHashMap.replaceAll((s, i) -> i *= multiplier);
-
                 for (Map.Entry<String, Integer> innerMapEntry : innerHashMap.entrySet()) {
                     String innerKey = innerMapEntry.getKey();
                     Integer innerValue = innerMapEntry.getValue();
                     Integer old = hashMap.getOrDefault(innerKey, 0);
-                    hashMap.put(innerKey, old + innerValue);
+                    hashMap.put(innerKey, old + innerValue * multiplier);
                 }
+            } else {
+                Map.Entry<String, Integer> elementWithCount = splitElementAndItsCount(token);
+                String elementName = elementWithCount.getKey();
+                Integer elementCount = elementWithCount.getValue();
+                Integer old = hashMap.getOrDefault(elementName, 0);
+                hashMap.put(elementName, old + elementCount);
             }
         }
-
         return hashMap;
     }
 
-    private static Map.Entry<String, Integer> splitMolecule(String token) {
-        //use SYMBOLS
-        //count should be 1 if its not there
-        return Map.entry("Mg", 3);
+    private static Map.Entry<String, Integer> splitElementAndItsCount(String token) {
+        int indexForSplit = -1;
+        for (int i = 0; i < token.length(); i++) {
+            char c = token.charAt(i);
+            if (Character.isDigit(c)) {
+                indexForSplit = i;
+                break;
+            }
+        }
+        if (indexForSplit == -1) {
+            return Map.entry(token, 1);
+        }
+        String name = token.substring(0, indexForSplit);
+        String count = token.substring(indexForSplit);
+        int elementCount = Integer.parseInt(count);
+        return Map.entry(name, elementCount);
     }
 
-    private static Map.Entry<String, Integer> splitFormula(String token) {
-        //return inner formula without brackets, count should be 1 if its not there
-        return Map.entry("ON(SO3)2", 2);
+    private static Map.Entry<String, Integer> stripAndSplitFormulaFromItsCount(String token) {
+        Map.Entry<Character, Integer> mostOuterBracket = Stream.of(
+                        Map.entry(OPENING_ROUND_BRACKET, token.indexOf(OPENING_ROUND_BRACKET)),
+                        Map.entry(OPENING_SQUARE_BRACKET, token.indexOf(OPENING_SQUARE_BRACKET)),
+                        Map.entry(OPENING_CURLY_BRACKET, token.indexOf(OPENING_CURLY_BRACKET))
+                )
+                .filter(entry -> entry.getValue() != -1)
+                .min(Map.Entry.comparingByValue(Integer::compare))
+                .orElseThrow(() -> new IllegalArgumentException("Failed to find opening bracket in inner formula"));
+        int indexOfClosingBracket = token.lastIndexOf(getClosingBracket(mostOuterBracket.getKey()));
+        if (indexOfClosingBracket == -1) {
+            throw new IllegalArgumentException("Could not find closing bracket for inner formula");
+        }
+        String innerFormula = token.substring(1, indexOfClosingBracket);
+        String countString = token.substring(indexOfClosingBracket + 1);
+        int count = 1;
+        if (!countString.isEmpty()) {
+            count = Integer.parseInt(countString);
+        }
+        return Map.entry(innerFormula, count);
     }
 
-    private static boolean isMulecule(String token) {
-        //use SYMBOLS
-        return false;
+    private static char getClosingBracket(char openingBracket) {
+        return switch (openingBracket) {
+            case OPENING_ROUND_BRACKET -> CLOSING_ROUND_BRACKET;
+            case OPENING_SQUARE_BRACKET -> CLOSING_SQUARE_BRACKET;
+            case OPENING_CURLY_BRACKET -> CLOSING_CURLY_BRACKET;
+            default -> throw new IllegalArgumentException("Wrong bracket");
+        };
+    }
+
+    private static boolean containsInnerFormula(String token) {
+        return token.contains(String.valueOf(OPENING_ROUND_BRACKET))
+                || token.contains(String.valueOf(OPENING_SQUARE_BRACKET))
+                || token.contains(String.valueOf(OPENING_CURLY_BRACKET));
     }
 
     private static class FormulaIterator implements Iterator<String> {
 
-        private final String tempFormula;
+        private final String formula;
         private int currentIndex;
 
         public FormulaIterator(String formula) {
-            tempFormula = formula;
+            this.formula = formula;
             currentIndex = 0;
         }
 
         @Override
         public boolean hasNext() {
-            return currentIndex < tempFormula.length();
+            return currentIndex < formula.length();
         }
 
         @Override
@@ -109,49 +132,32 @@ class ParseMolecule {
             if (!hasNext()) {
                 throw new NoSuchElementException("No more elements in the formula");
             }
-
             StringBuilder tokenBuilder = new StringBuilder();
-            char currentChar = tempFormula.charAt(currentIndex);
-
-            // Handling molecule with count
+            char currentChar = formula.charAt(currentIndex);
             if (Character.isUpperCase(currentChar)) {
                 tokenBuilder.append(currentChar);
                 currentIndex++;
-
-                // Collect the entire molecule, including any lowercase characters
-                while (currentIndex < tempFormula.length() &&
-                        (Character.isLowerCase(tempFormula.charAt(currentIndex)) || Character.isDigit(tempFormula.charAt(currentIndex)))) {
-                    tokenBuilder.append(tempFormula.charAt(currentIndex));
+                while (currentIndex < formula.length() &&
+                        (Character.isLowerCase(formula.charAt(currentIndex)) || Character.isDigit(formula.charAt(currentIndex)))) {
+                    tokenBuilder.append(formula.charAt(currentIndex));
                     currentIndex++;
                 }
-            }
-            // Handling inner formula with count
-            else if (isOpeningBracket(currentChar)) {
+            } else if (isOpeningBracket(currentChar)) {
                 int openBracketIndex = currentIndex;
-                int closingBracketIndex = findClosingBracket(tempFormula, openBracketIndex);
-                tokenBuilder.append(tempFormula, openBracketIndex, closingBracketIndex + 1);
+                int closingBracketIndex = findClosingBracket(formula, openBracketIndex);
+                tokenBuilder.append(formula, openBracketIndex, closingBracketIndex + 1);
                 currentIndex = closingBracketIndex + 1;
-                // Collect the entire molecule, including any lowercase characters
-                while (currentIndex < tempFormula.length() && Character.isDigit(tempFormula.charAt(currentIndex))) {
-                    tokenBuilder.append(tempFormula.charAt(currentIndex));
+                while (currentIndex < formula.length() && Character.isDigit(formula.charAt(currentIndex))) {
+                    tokenBuilder.append(formula.charAt(currentIndex));
                     currentIndex++;
                 }
             } else {
-                throw new IllegalStateException("Invalid character encountered in formula");
+                throw new IllegalArgumentException("Invalid character encountered in formula");
             }
 
             return tokenBuilder.toString();
         }
 
-        private boolean isOpeningBracket(char c) {
-            return c == '(' || c == '[' || c == '{';
-        }
-
-        private boolean isClosingBracket(char c) {
-            return c == ')' || c == ']' || c == '}';
-        }
-
-        // Helper method to find the index of the closing bracket for a given opening bracket
         private int findClosingBracket(String formula, int openBracketIndex) {
             int openBracketCount = 1;
             int closingBracketIndex = openBracketIndex + 1;
@@ -166,9 +172,17 @@ class ParseMolecule {
                 closingBracketIndex++;
             }
             if (openBracketCount != 0) {
-                throw new IllegalStateException("Invalid inner formula encountered in formula");
+                throw new IllegalArgumentException("Invalid inner formula encountered in formula");
             }
             return closingBracketIndex - 1;
+        }
+
+        private boolean isOpeningBracket(char c) {
+            return c == OPENING_ROUND_BRACKET || c == OPENING_SQUARE_BRACKET || c == OPENING_CURLY_BRACKET;
+        }
+
+        private boolean isClosingBracket(char c) {
+            return c == CLOSING_ROUND_BRACKET || c == CLOSING_SQUARE_BRACKET || c == CLOSING_CURLY_BRACKET;
         }
     }
 }
--- src/test/java/molecule/to/atoms/ParseMoleculeTest.java
+++ src/test/java/molecule/to/atoms/ParseMoleculeTest.java
@@ -8,21 +8,40 @@ import java.util.Map;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class ParseMoleculeTest {
 
-    private static Stream<Arguments> generateRandomTestCases() {
+    @ParameterizedTest(name = "Should return {1} for \"{0}\"")
+    @MethodSource("generateTestCases")
+    void testMolecule(String formula, Map<String, Integer> expected) {
+        assertEquals(expected, ParseMolecule.getAtoms(formula));
+    }
+
+    @ParameterizedTest(name = "Should throw IllegalArgumentException for \"{0}\"")
+    @MethodSource("generateMoreTestCases")
+    void testExceptions(String formula) {
+        assertThrows(IllegalArgumentException.class, () -> ParseMolecule.getAtoms(formula));
+    }
+
+    private static Stream<Arguments> generateTestCases() {
         return Stream.of(
                 Arguments.of("H2O", Map.of("H", 2, "O", 1)),
                 Arguments.of("Mg(OH)2", Map.of("Mg", 1, "H", 2, "O", 2)),
-                Arguments.of("K4[ON(SO3)2]2", Map.of("K", 4, "O", 14, "N", 2, "S", 4))
+                Arguments.of("K4[ON(SO3)2]2", Map.of("K", 4, "O", 14, "N", 2, "S", 4)),
+                Arguments.of("K(N)", Map.of("K", 1, "N", 1)),
+                Arguments.of("C6H12O6", Map.of("C", 6, "H", 12, "O", 6)),
+                Arguments.of("{[Co(NH3)4(OH)2]3Co}(SO4)3", Map.of("Co", 4, "N", 12, "H", 42, "O", 18, "S", 3)),
+                Arguments.of("Pd[P(C6H5)3]4", Map.of("Pd", 1, "C", 72, "H", 60, "P", 4))
         );
     }
 
-
-    @ParameterizedTest(name = "Should return {1} for \"{0}\"")
-    @MethodSource("generateRandomTestCases")
-    void testMolecule(String formula, Map<String, Integer> expected) {
-        assertEquals(expected, ParseMolecule.getAtoms(formula));
+    private static Stream<Arguments> generateMoreTestCases() {
+        return Stream.of(
+                Arguments.of("pie"),
+                Arguments.of("Mg(OH"),
+                Arguments.of("MgOH)2"),
+                Arguments.of("Mg(OH]2")
+        );
     }
 }
