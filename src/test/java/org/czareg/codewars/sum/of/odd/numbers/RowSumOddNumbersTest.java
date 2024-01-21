3c4f083f3a91aa9bd69a0760b1a306825363f98b
Czareg <czarecoo@gmail.com>
2024-01-21 13:31:33 +0100

    Added random tests to prev kata

--- src/test/java/org/czareg/codewars/sum/of/odd/numbers/RowSumOddNumbersTest.java
+++ src/test/java/org/czareg/codewars/sum/of/odd/numbers/RowSumOddNumbersTest.java
@@ -2,6 +2,8 @@ package org.czareg.codewars.sum.of.odd.numbers;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Random;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class RowSumOddNumbersTest {
@@ -11,5 +13,14 @@ class RowSumOddNumbersTest {
         assertEquals(1, RowSumOddNumbers.rowSumOddNumbers(1));
         assertEquals(74088, RowSumOddNumbers.rowSumOddNumbers(42));
         assertEquals(27, RowSumOddNumbers.rowSumOddNumbers(3));
+    }
+
+    @Test
+    void randomTests() {
+        Random rnd = new Random();
+        for (int i = 0; i < 50; i++) {
+            int n = rnd.nextInt(500) + 1;
+            assertEquals((int) Math.pow(n, 3), RowSumOddNumbers.rowSumOddNumbers(n));
+        }
     }
 }
