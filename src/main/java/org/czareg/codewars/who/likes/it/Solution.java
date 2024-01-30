99aebef59a08a78430ebc8f1d7d019caaa077d9b
Czareg <czarecoo@gmail.com>
2024-01-30 23:12:46 +0100

    Done 1 kata

---
+++ src/main/java/org/czareg/codewars/who/likes/it/Solution.java
@@ -0,0 +1,29 @@
+package org.czareg.codewars.who.likes.it;
+
+import lombok.experimental.UtilityClass;
+
+/*
+You probably know the "like" system from Facebook and other pages. People can "like" blog posts, pictures or other items. We want to create the text that should be displayed next to such an item.
+
+Implement the function which takes an array containing the names of people that like an item. It must return the display text as shown in the examples:
+
+[]                                -->  "no one likes this"
+["Peter"]                         -->  "Peter likes this"
+["Jacob", "Alex"]                 -->  "Jacob and Alex like this"
+["Max", "John", "Mark"]           -->  "Max, John and Mark like this"
+["Alex", "Jacob", "Mark", "Max"]  -->  "Alex, Jacob and 2 others like this"
+Note: For 4 or more names, the number in "and 2 others" simply increases.
+ */
+@UtilityClass
+public class Solution {
+
+    public static String whoLikesIt(String... names) {
+        return switch (names.length) {
+            case 0 -> "no one likes this";
+            case 1 -> "%s likes this".formatted(names[0]);
+            case 2 -> "%s and %s like this".formatted(names[0], names[1]);
+            case 3 -> "%s, %s and %s like this".formatted(names[0], names[1], names[2]);
+            default -> "%s, %s and %s others like this".formatted(names[0], names[1], names.length - 2);
+        };
+    }
+}
---
+++ src/test/java/org/czareg/codewars/who/likes/it/SolutionTest.java
@@ -0,0 +1,51 @@
+package org.czareg.codewars.who.likes.it;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SolutionTest {
+
+    private static final String[] NAMES = new String[]{"Brian", "Marilyn", "Pamela", "Timothy", "Jesse", "Heather", "Terry", "Carlos",
+            "Bonnie", "Randy", "Lillian", "Emily", "Louis", "Chris", "Howard", "Helen", "Ralph", "Jennifer", "Mark", "Laura",
+            "Jason", "Shirley", "Diane", "Phillip", "David", "Joan", "Wanda", "Jimmy", "Carl", "Betty", "Adam", "Lawrence",
+            "Kathleen", "Mildred", "Rose", "Tina", "Jose", "Keith", "Janice", "Maria", "Kenneth", "Arthur", "James", "Catherine",
+            "Henry", "Denise", "Ruby", "Cynthia", "Anthony", "Jeffrey", "Eugene", "Dorothy", "Lori", "Bobby", "Peter", "Alice",
+            "Eric", "Wayne", "Phyllis", "Roger", "Clarence", "Scott", "John", "Philip", "Teresa", "Andrea", "Douglas", "Earl",
+            "Melissa", "Benjamin", "Rebecca", "Michelle", "Alan", "Brenda", "William", "Frank", "Matthew", "George", "Anna",
+            "Cheryl", "Roy", "Paula", "Tammy", "Gerald", "Christina", "Russell", "Kelly", "Albert", "Donna", "Todd", "Jessica",
+            "Kimberly", "Johnny", "Dennis", "Jack", "Doris", "Martha", "Stephanie", "Stephen", "Christine"};
+
+    @Test
+    void staticTests() {
+        assertEquals("no one likes this", Solution.whoLikesIt());
+        assertEquals("Peter likes this", Solution.whoLikesIt("Peter"));
+        assertEquals("Jacob and Alex like this", Solution.whoLikesIt("Jacob", "Alex"));
+        assertEquals("Max, John and Mark like this", Solution.whoLikesIt("Max", "John", "Mark"));
+        assertEquals("Alex, Jacob and 2 others like this", Solution.whoLikesIt("Alex", "Jacob", "Mark", "Max"));
+        assertEquals("Brian, Marilyn and 98 others like this", Solution.whoLikesIt(NAMES));
+    }
+
+    @Test
+    void randomTests() {
+        Random rand = new Random();
+        for (int i = 0; i < 24; i++) {
+            int r = rand.nextInt(100);
+            String[] randNames = new String[r];
+            for (int j = 0; j < r; j++) randNames[j] = NAMES[rand.nextInt(100)];
+            assertEquals(whoLikesIt(randNames), Solution.whoLikesIt(randNames));
+        }
+    }
+
+    private String whoLikesIt(String... names) {
+        return switch (names.length) {
+            case 0 -> "no one likes this";                //feels bad man
+            case 1 -> names[0] + " likes this";
+            case 2 -> String.format("%s and %s like this", names[0], names[1]);
+            case 3 -> String.format("%s, %s and %s like this", names[0], names[1], names[2]);
+            default -> String.format("%s, %s and %d others like this", names[0], names[1], names.length - 2);
+        };
+    }
+}
