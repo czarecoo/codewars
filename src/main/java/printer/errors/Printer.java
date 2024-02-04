1101198a09364923ad7cca0a7890079b4a67da94
Czareg <czarecoo@gmail.com>
2024-02-04 18:30:41 +0100

    Done 1 kata

---
+++ src/main/java/printer/errors/Printer.java
@@ -0,0 +1,31 @@
+package printer.errors;
+
+import lombok.experimental.UtilityClass;
+
+/*
+In a factory a printer prints labels for boxes. For one kind of boxes the printer has to use colors which, for the sake of simplicity, are named with letters from a to m.
+
+The colors used by the printer are recorded in a control string. For example a "good" control string would be aaabbbbhaijjjm meaning that the printer used three times color a, four times color b, one time color h then one time color a...
+
+Sometimes there are problems: lack of colors, technical malfunction and a "bad" control string is produced e.g. aaaxbbbbyyhwawiwjjjwwm with letters not from a to m.
+
+You have to write a function printer_error which given a string will return the error rate of the printer as a string representing a rational whose numerator is the number of errors and the denominator the length of the control string. Don't reduce this fraction to a simpler expression.
+
+The string has a length greater or equal to one and contains only letters from ato z.
+
+Examples:
+s="aaabbbbhaijjjm"
+printer_error(s) => "0/14"
+
+s="aaaxbbbbyyhwawiwjjjwwm"
+printer_error(s) => "8/22"
+ */
+@UtilityClass
+public class Printer {
+
+    public static String printerError(String s) {
+        int errorLength = s.replaceAll("[a-m]", "").length();
+        int wholeLength = s.length();
+        return "%s/%s".formatted(errorLength, wholeLength);
+    }
+}
---
+++ src/test/java/printer/errors/PrinterTest.java
@@ -0,0 +1,62 @@
+package printer.errors;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class PrinterTest {
+
+    private static int randInt(int min, int max) {
+        return min + (int) (Math.random() * ((max - min) + 1));
+    }
+
+    public static String doEx() {
+        int i = 0;
+        StringBuilder res = new StringBuilder();
+        int n;
+        int k = randInt(10, 500);
+        while (i < 3 * k / 2) {
+            n = randInt(97, 109);
+            res.append((char) (n));
+            i++;
+        }
+        while (i < 2 * k) {
+            if (i % 17 == 0) n = randInt(110, 122);
+            else n = randInt(97, 109);
+            res.append((char) (n));
+            i++;
+        }
+        return res.toString();
+    }
+
+    public static String printerErrorSol(String s) {
+        int cnt = 0;
+        int l = s.length();
+        for (int i = 0; i < l; i++) {
+            int c = s.charAt(i);
+            if (c > 109 && c <= 122)
+                cnt++;
+        }
+        return cnt + "/" + l;
+    }
+
+    // ---------------------------
+    @Test
+    void test() {
+        String s = "aaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbmmmmmmmmmmmmmmmmmmmxyz";
+        assertEquals("3/56", Printer.printerError(s));
+        s = "kkkwwwaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbmmmmmmmmmmmmmmmmmmmxyz";
+        assertEquals("6/60", Printer.printerError(s));
+        s = "kkkwwwaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbmmmmmmmmmmmmmmmmmmmxyzuuuuu";
+        assertEquals("11/65", Printer.printerError(s));
+    }
+
+    @Test
+    void test1() {
+
+        for (int i = 0; i < 200; i++) {
+            String s = doEx();
+            assertEquals(printerErrorSol(s), Printer.printerError(s));
+        }
+    }
+}
