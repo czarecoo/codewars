a3ffef774eae6e1e34cf1936ea86f325b78d251e
Czareg <czarecoo@gmail.com>
2024-02-17 12:47:31 +0100

    fixed test method order

--- src/test/java/org/czareg/codewars/combine/strings/StringCombinerTest.java
+++ src/test/java/org/czareg/codewars/combine/strings/StringCombinerTest.java
@@ -1,13 +1,16 @@
 package org.czareg.codewars.combine.strings;
 
+import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 
 import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class StringCombinerTest {
 
     @Test
