b7e2d7d0b4a2833a043c74c0d9571c0e5eeebc91
Czareg <czarecoo@gmail.com>
2024-04-13 19:06:24 +0200

    Added random tests for last kata

--- src/test/java/org/czareg/codewars/continued/fraction/FracterTest.java
+++ src/test/java/org/czareg/codewars/continued/fraction/FracterTest.java
@@ -1,15 +1,19 @@
 package org.czareg.codewars.continued.fraction;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class FracterTest {
 
     @Test
+    @Order(1)
     @DisplayName("Basic tests")
     void basicTests() {
         int[] numerators = new int[]{311, 761, 1721};
@@ -27,6 +31,7 @@ class FracterTest {
     }
 
     @Test
+    @Order(1)
     @DisplayName("Tests for divisors")
     void divTests() {
         int[] numerators = new int[]{1089, 11011, 1173};
@@ -44,10 +49,37 @@ class FracterTest {
     }
 
     @Test
+    @Order(1)
     @DisplayName("Test for zero")
     void zeroTest() {
         int[] actual = Fracter.continuedFraction(0, 15);
         String msg = String.format("Input : numerator=0, denominator=15. Actual %s should equal expected []", Arrays.toString(actual));
         assertArrayEquals(new int[0], actual, msg);
+    }
+
+    @Test
+    @Order(2)
+    @DisplayName("100 random tests")
+    void randomTests() {
+        Random random = new Random();
+        for (int i = 1; i <= 100; i++) {
+            int numerator = random.nextInt(100 * i);
+            int denominator = random.nextInt(100 * i) + 1;
+            int[] expected = solution(numerator, denominator);
+            int[] actual = Fracter.continuedFraction(numerator, denominator);
+            String msg = String.format("Input : numerator=%s, denominator=%s. Actual %s should equal expected %s", numerator, denominator, Arrays.toString(actual), Arrays.toString(expected));
+            assertArrayEquals(expected, actual, msg);
+        }
+    }
+
+    public static int[] solution(int numerator, int denominator) {
+        List<Integer> parts = new ArrayList<>();
+        while (numerator != 0 && denominator != 0) {
+            parts.add(numerator / denominator);
+            int temp = denominator;
+            denominator = numerator % temp;
+            numerator = temp;
+        }
+        return parts.stream().mapToInt(i -> i).toArray();
     }
 }
