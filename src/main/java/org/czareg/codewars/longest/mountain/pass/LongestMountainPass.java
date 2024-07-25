10edf7073db64d3910c40220bf6b3524c029cbc1
Czareg <czarecoo@gmail.com>
2024-07-25 13:20:27 +0200

    Working on kata

---
+++ src/main/java/org/czareg/codewars/longest/mountain/pass/LongestMountainPass.java
@@ -0,0 +1,57 @@
+package org.czareg.codewars.longest.mountain.pass;
+
+import lombok.experimental.UtilityClass;
+
+/*
+You are an adventurous hiker planning to traverse a mountain range. The mountain range is represented by an array of integers,
+where each element corresponds to the height of a mountain at that position.
+Your goal is to find the longest mountain pass you can take based on your initial energy level.
+
+Problem Description
+You are given an array of mountains, where each element represents the height of the mountain.
+A mountain pass is defined as a subarray of the mountain array. The length of a mountain pass is the length of the subarray.
+You have an initial energy level E.
+You start at any initial index of your choice.
+Going up a mountain (i.e., moving from a lower height to a higher height) costs you energy equal to the difference in heights.
+Going down a mountain or staying at the same height does not cost you any energy.
+You can only move to the mountains on your right (i.e., the next index).
+Your task is to find the longest mountain pass you can take based on your initial energy level.
+Return the length of the longest mountain pass and the initial index from where you should start.
+
+Input
+mountains: An array of integers representing the heights of the mountains.
+E: An integer representing your initial energy level.
+Note: The length of the mountain array can be very large, up to 10^7.
+
+Output
+Return a tuple (max_length, start_index), where:
+
+max_length: The length of the longest mountain pass you can take.
+start_index: The initial index from where you should start the mountain pass.
+If there are multiple mountain passes with the same length, return the one with the smallest initial index.
+ */
+@UtilityClass
+class LongestMountainPass {
+
+    static Result longestMountainPass(int[] mountains, int initialEnergyLevel) {
+        int maxLength = 0;
+        int startIdx = 0;
+
+        for (int i = 0; i < mountains.length; i++) {
+            int energy = initialEnergyLevel;
+            int length = 0;
+            int j = i;
+            while (j < mountains.length - 1 && (energy - (mountains[j + 1] - mountains[j])) >= 0) {
+                energy -= (mountains[j + 1] - mountains[j]);
+                j++;
+                length++;
+            }
+            if (length > maxLength) {
+                maxLength = length;
+                startIdx = i;
+            }
+        }
+
+        return new Result(maxLength, startIdx);
+    }
+}
---
+++ src/test/java/org/czareg/codewars/longest/mountain/pass/LongestMountainPassTest.java
@@ -0,0 +1,22 @@
+package org.czareg.codewars.longest.mountain.pass;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class LongestMountainPassTest {
+
+    @Test
+    void basicTests() {
+        assertEquals(new Result(10, 0), LongestMountainPass.longestMountainPass(new int[]{10, 9, 8, 7, 6, 5, 4, 3, 2, 3}, 1));
+        assertEquals(new Result(7, 0), LongestMountainPass.longestMountainPass(new int[]{9, 1, 2, 3, 4, 5, 6, 9}, 7));
+        assertEquals(new Result(9, 1), LongestMountainPass.longestMountainPass(new int[]{1, 8, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 7));
+    }
+
+    @Test
+    void edgeCaseTests() {
+        assertEquals(new Result(0, 0), LongestMountainPass.longestMountainPass(new int[]{}, 0));
+        assertEquals(new Result(3, 0), LongestMountainPass.longestMountainPass(new int[]{10, 10, 10}, 0));
+        assertEquals(new Result(1, 0), LongestMountainPass.longestMountainPass(new int[]{1, 2, 3, 4, 5}, 0));
+    }
+}
