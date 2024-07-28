3d74ae2042a1f0fb1756df3a7aa3db2073ad36ef
Czareg <czarecoo@gmail.com>
2024-07-28 10:43:06 +0200

    Improved last kata. On codewars the average time improved 10090ms -> 2510ms.

--- src/main/java/org/czareg/codewars/find/the/parity/outlier/FindOutlier.java
+++ src/main/java/org/czareg/codewars/find/the/parity/outlier/FindOutlier.java
@@ -3,12 +3,13 @@ package org.czareg.codewars.find.the.parity.outlier;
 import lombok.experimental.UtilityClass;
 
 import java.util.Arrays;
-import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 /*
-You are given an array (which will have a length of at least 3, but could be very large) containing integers. The array is either entirely comprised of odd integers or entirely comprised of even integers except for a single integer N. Write a method that takes the array as an argument and returns this "outlier" N.
+You are given an array (which will have a length of at least 3, but could be very large) containing integers.
+The array is either entirely comprised of odd integers or entirely comprised of even integers except for a single integer N.
+Write a method that takes the array as an argument and returns this "outlier" N.
 
 Examples
 [2, 4, 0, 100, 4, 11, 2602, 36] -->  11 (the only odd number)
@@ -19,11 +20,26 @@ Examples
 public class FindOutlier {
 
     static int find(int[] integers) {
-        Map<Boolean, List<Integer>> evenOdds = Arrays.stream(integers)
+        boolean areEven = Arrays.stream(integers)
+                .limit(3)
                 .boxed()
-                .collect(Collectors.partitioningBy(num -> num % 2 == 0));
-        List<Integer> evens = evenOdds.get(Boolean.TRUE);
-        List<Integer> odds = evenOdds.get(Boolean.FALSE);
-        return evens.size() < odds.size() ? evens.getFirst() : odds.getFirst();
+                .collect(Collectors.partitioningBy(FindOutlier::isEven, Collectors.counting()))
+                .entrySet()
+                .stream()
+                .min(Map.Entry.comparingByValue())
+                .orElseThrow()
+                .getKey();
+        return Arrays.stream(integers)
+                .filter(areEven ? FindOutlier::isEven : FindOutlier::isOdd)
+                .findFirst()
+                .orElseThrow();
+    }
+
+    private static boolean isOdd(int number) {
+        return !isEven(number);
+    }
+
+    private static boolean isEven(int number) {
+        return number % 2 == 0;
     }
 }
--- src/test/java/org/czareg/codewars/find/the/parity/outlier/FindOutlierTest.java
+++ src/test/java/org/czareg/codewars/find/the/parity/outlier/FindOutlierTest.java
@@ -1,15 +1,81 @@
 package org.czareg.codewars.find.the.parity.outlier;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.IntStream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class FindOutlierTest {
 
     @Test
-    void sampleTests() {
-        assertEquals(3, FindOutlier.find(new int[]{2, 6, 8, -10, 3}));
-        assertEquals(206847684, FindOutlier.find(new int[]{206847684, 1056521, 7, 17, 1901, 21104421, 7, 1, 35521, 1, 7781}));
-        assertEquals(0, FindOutlier.find(new int[]{Integer.MAX_VALUE, 0, 1}));
+    @DisplayName("Fixed tests")
+    @Order(1)
+    void fixedTests() {
+        int[] ints1 = {2, 6, 8, 10, 3};
+        int[] ints2 = {2, 6, 8, 200, 700, 1, 84, 10, 4};
+        int[] ints3 = {17, 6, 8, 10, 6, 12, 24, 36};
+        int[] ints4 = {2, 1, 7, 17, 19, 211, 7};
+        int[] ints5 = {1, 1, 1, 1, 1, 44, 7, 7, 7, 7, 7, 7, 7, 7};
+        int[] ints6 = {3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 35, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 7, 7, 7, 7, 1000};
+        int[] ints7 = {2, -6, 8, -10, -3};
+        int[] ints8 = {2, 6, 8, 2, -66, 34, -35, 66, 700, 1002, -84, 10, 4};
+        int[] ints9 = {-1 * Integer.MAX_VALUE, -18, 6, -8, -10, 6, 12, -24, 36};
+        int[] ints10 = {-20, 1, 7, 17, 19, 211, 7};
+        int[] ints11 = {1, 1, -1, 1, 1, -44, 7, 7, 7, 7, 7, 7, 7, 7};
+        int[] ints12 = {1, 0, 0};
+        int[] ints13 = {3, 7, -99, 81, 90211, 0, 7};
+        int[] ints14 = {-5, -6, -7, -3, -11, -3, -9};
+
+        assertEquals(3, FindOutlier.find(ints1));
+        assertEquals(1, FindOutlier.find(ints2));
+        assertEquals(17, FindOutlier.find(ints3));
+        assertEquals(2, FindOutlier.find(ints4));
+        assertEquals(44, FindOutlier.find(ints5));
+        assertEquals(1000, FindOutlier.find(ints6));
+        assertEquals(-3, FindOutlier.find(ints7));
+        assertEquals(-35, FindOutlier.find(ints8));
+        assertEquals(-1 * Integer.MAX_VALUE, FindOutlier.find(ints9));
+        assertEquals(-20, FindOutlier.find(ints10));
+        assertEquals(-44, FindOutlier.find(ints11));
+        assertEquals(1, FindOutlier.find(ints12));
+        assertEquals(0, FindOutlier.find(ints13));
+        assertEquals(-6, FindOutlier.find(ints14));
+    }
+
+    @Test
+    @DisplayName("Small random tests")
+    @Order(2)
+    void randomTests1() {
+        for (int run = 0; run < 50; ++run)
+            randomTest(50, 3, 10);
+    }
+
+    @Test
+    @DisplayName("Medium random tests")
+    @Order(3)
+    void randomTests2() {
+        for (int run = 0; run < 30; ++run)
+            randomTest(50000, 300, 400);
+    }
+
+    @Test
+    @DisplayName("Big random tests")
+    @Order(4)
+    void randomTests3() {
+        for (int run = 0; run < 10; ++run) {
+            randomTest(5_000_000, 9_000_000, 10_000_000);
+        }
+    }
+
+    private void randomTest(int valueRange, int minSize, int maxSize) {
+        var rnd = ThreadLocalRandom.current();
+        int parity = rnd.nextBoolean() ? 1 : 0;
+        var input = IntStream.generate(() -> 2 * rnd.nextInt(-valueRange, valueRange) + parity).limit(rnd.nextLong(minSize, maxSize)).toArray();
+        int expected = 2 * rnd.nextInt(-valueRange, valueRange) + 1 + parity;
+        input[rnd.nextInt(input.length)] = expected;
+        assertEquals(expected, FindOutlier.find(input));
     }
 }
